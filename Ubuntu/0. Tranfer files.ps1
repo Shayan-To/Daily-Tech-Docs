0c1633236758fd4381e71322c2904a747f5bde51
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-06-21 05:15:02 +0430

    Commit

--- Ubuntu/0. Tranfer files.ps1
+++ Ubuntu/0. Tranfer files.ps1
@@ -6,6 +6,7 @@ If ($Dest -ne "Server" -and $Dest -ne "Local")
 }
 
 $Server = "root@104.251.217.218"
+$SocketFile = "/tmp/ssh_socket"
 
 Write-Host
 If ($Dest -eq "Server")
@@ -27,116 +28,131 @@ While ($True)
     }
 }
 
-While ($True)
+If ($Dest -eq "Server")
 {
-    Write-Host
-    $Path = Read-Host "Path [exit]"
+    Write-Host "Opening connection to the server..."
+    bash -c "rm -f $SocketFile"
+    Start-Process bash -ArgumentList "-c 'ssh -N -M -o 'ServerAliveInterval=60' -o 'ControlPath=$SocketFile' $Server'" -NoNewWindow
+}
 
-	$Path = $Path.Trim()
-
-    If ($Path.Length -eq 0)
+Try
+{
+    While ($True)
     {
-        Break
-    }
+        Write-Host
+        $Path = Read-Host "Path [exit]"
 
-    If ($Path[0] -ne "/"[0])
-    {
-        If ($Path[0] -eq '"'[0] -and $Path[$Path.Length - 1] -eq '"'[0])
+	    $Path = $Path.Trim()
+
+        If ($Path.Length -eq 0)
         {
-            $Path = $Path.Substring(1, $Path.Length - 2)
+            Break
         }
 
-        $Path = $Path.Substring($Path.IndexOf("#"[0]) + 1)
-    }
-    $Path = $Path.Replace("\"[0], "/"[0])
-    Write-Host "Path: $Path"
-    $Path = $Path.Replace("\", "\\").Replace("'", "\'")
-
-    $Commands = [System.Collections.Generic.List[String]]::New()
-
-    $Parent = $Path.Substring(0, $Path.LastIndexOf("/"[0]) + 1)
-
-    If ($SR -eq "S")
-    {
-        $Command = "mkdir -p `$'$Parent'"
-        If ($Dest -eq "Server")
+        If ($Path[0] -ne "/"[0])
         {
-            $Command = $Command.Replace("\", "\\").Replace("'", "\'")
-            $Commands.Add("ssh $Server `$'$Command'")
-        }
-        Else
-        {
-            $Commands.Add("echo ' ' | sudo -Sp '' $Command")
-        }
-    }
-    Else
-    {
-        $Commands.Add("mkdir -p `$'.$Parent'")
-    }
-
-    If ($Dest -eq "Server")
-    {
-        If ($SR -eq "S")
-        {
-            $Commands.Add("scp `$'.$Path' `$'${Server}:$Path'")
-        }
-        Else
-        {
-            $Commands.Add("scp `$'${Server}:$Path' `$'.$Path'")
-        }
-    }
-    Else
-    {
-        If ($SR -eq "S")
-        {
-            $Commands.Add("echo ' ' | sudo -Sp '' cp `$'.$Path' `$'$Path'")
-        }
-        Else
-        {
-            $Commands.Add("echo ' ' | sudo -Sp '' cp `$'$Path' `$'.$Path'")
-        }
-    }
-
-    If ($SR -eq "S")
-    {
-        If ($Dest -eq "Server")
-        {
-            $Permission = Read-Host "Permission (chmod - rwx) [nothing]"
-            $Permission = $Permission.Trim()
-        }
-        Else
-        {
-            $Permission = Read-Host "Permission (chmod - rwx) [777]"
-            $Permission = $Permission.Trim()
-            If ($Permission.Length -ne 0)
+            If ($Path[0] -eq '"'[0] -and $Path[$Path.Length - 1] -eq '"'[0])
             {
-                $Permission = "777"
+                $Path = $Path.Substring(1, $Path.Length - 2)
             }
-        }
 
-        If ($Permission.Length -ne 0)
+            $Path = $Path.Substring($Path.IndexOf("#"[0]) + 1)
+        }
+        $Path = $Path.Replace("\"[0], "/"[0])
+        Write-Host "Path: $Path"
+        $Path = $Path.Replace("\", "\\").Replace("'", "\'")
+
+        $Commands = [System.Collections.Generic.List[String]]::New()
+
+        $Parent = $Path.Substring(0, $Path.LastIndexOf("/"[0]) + 1)
+
+        If ($SR -eq "S")
         {
-            $Command = "chmod $Permission `$'$Path'"
+            $Command = "mkdir -p `$'$Parent'"
             If ($Dest -eq "Server")
             {
                 $Command = $Command.Replace("\", "\\").Replace("'", "\'")
-                $Commands.Add("ssh $Server `$'$Command'")
+                $Commands.Add("ssh -o 'ControlPath=$SocketFile' $Server `$'$Command'")
             }
             Else
             {
                 $Commands.Add("echo ' ' | sudo -Sp '' $Command")
             }
         }
+        Else
+        {
+            $Commands.Add("mkdir -p `$'.$Parent'")
+        }
+
+        If ($Dest -eq "Server")
+        {
+            If ($SR -eq "S")
+            {
+                $Commands.Add("scp -o 'ControlPath=$SocketFile' `$'.$Path' `$'${Server}:$Path'")
+            }
+            Else
+            {
+                $Commands.Add("scp -o 'ControlPath=$SocketFile' `$'${Server}:$Path' `$'.$Path'")
+            }
+        }
+        Else
+        {
+            If ($SR -eq "S")
+            {
+                $Commands.Add("echo ' ' | sudo -Sp '' cp `$'.$Path' `$'$Path'")
+            }
+            Else
+            {
+                $Commands.Add("echo ' ' | sudo -Sp '' cp `$'$Path' `$'.$Path'")
+            }
+        }
+
+        If ($SR -eq "S")
+        {
+            If ($Dest -eq "Server")
+            {
+                $Permission = Read-Host "Permission (chmod - rwx) [nothing]"
+                $Permission = $Permission.Trim()
+            }
+            Else
+            {
+                $Permission = Read-Host "Permission (chmod - rwx) [777]"
+                $Permission = $Permission.Trim()
+                If ($Permission.Length -ne 0)
+                {
+                    $Permission = "777"
+                }
+            }
+
+            If ($Permission.Length -ne 0)
+            {
+                $Command = "chmod $Permission `$'$Path'"
+                If ($Dest -eq "Server")
+                {
+                    $Command = $Command.Replace("\", "\\").Replace("'", "\'")
+                    $Commands.Add("ssh -o 'ControlPath=$SocketFile' $Server `$'$Command'")
+                }
+                Else
+                {
+                    $Commands.Add("echo ' ' | sudo -Sp '' $Command")
+                }
+            }
+        }
+
+        Write-Host
+        Write-Host "Commands:"
+        $Commands | % {Write-Host "    $_"}
+
+        Pause
+
+        $Commands | % {Write-Host; Write-Host $_; bash -c "$_"}
+
+        Write-Host
+        Write-Host "-----"
     }
-
-    Write-Host
-    Write-Host "Commands:"
-    $Commands | % {Write-Host "    $_"}
-
-    Pause
-
-    $Commands | % {Write-Host; Write-Host $_; bash -c "$_"}
-
-    Write-Host
-    Write-Host "-----"
+}
+Finally
+{
+    Write-Host "Closing connection to the server..."
+    bash -c "ssh -o 'ControlPath=$SocketFile' -O stop $Server"
 }
