1544de66649085f9f3b2447c327f35a8408d8acd
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-12-16 10:18:50 +0330

    Commit.

--- Ubuntu/0. Tranfer files.ps1
+++ Ubuntu/0. Tranfer files.ps1
@@ -1,5 +1,34 @@
 Param($Dest)
 
+Function FindAndSet-BashAndGit
+{
+    $Global:Git = 'git'
+    $Global:Bash = 'bash'
+
+    If ((Get-Command $Global:Git -ErrorAction SilentlyContinue) -EQ $Null)
+    {
+        $Global:Git = $Null
+    }
+    If ((Get-Command $Global:Bash -ErrorAction SilentlyContinue) -EQ $Null)
+    {
+        $Global:Bash = $Null
+    }
+
+    If (Test-Path HKLM:\SOFTWARE\GitForWindows)
+    {
+        $GitInstallPath = Get-ItemProperty HKLM:\SOFTWARE\GitForWindows | % InstallPath
+        $Global:Git = Join-Path $GitInstallPath 'bin\git.exe'
+        $Global:Bash = Join-Path $GitInstallPath 'bin\bash.exe'
+    }
+    ElseIf (Test-Path HKLM:\SOFTWARE\WOW6432Node\GitForWindows)
+    {
+        $GitInstallPath = Get-ItemProperty HKLM:\SOFTWARE\WOW6432Node\GitForWindows | % InstallPath
+        $Global:Git = Join-Path $GitInstallPath 'bin\git.exe'
+        $Global:Bash = Join-Path $GitInstallPath 'bin\bash.exe'
+    }
+}
+FindAndSet-BashAndGit
+
 If ($Dest -ne "Server" -and $Dest -ne "Local")
 {
     Exit
@@ -31,12 +60,12 @@ While ($True)
 If ($Dest -eq "Server")
 {
     Write-Host "Opening connection to the server..."
-    bash -c "rm -f $SocketFile"
+    & $Global:Bash -c "rm -f $SocketFile"
     Start-Job `
     {
         Param($Path, $SocketFile, $Server)
         Set-Location $Path
-        bash -c "ssh -N -M -o 'ServerAliveInterval=60' -o 'ControlPath=$SocketFile' $Server"
+        & $Global:Bash -c "ssh -N -M -o 'ServerAliveInterval=60' -o 'ControlPath=$SocketFile' $Server"
     } -ArgumentList $PWD, $SocketFile, $Server
 }
 
@@ -150,7 +179,7 @@ Try
 
         Pause
 
-        $Commands | % {Write-Host; Write-Host $_; bash -c "$_"}
+        $Commands | % {Write-Host; Write-Host $_; & $Global:Bash -c "$_"}
 
         Write-Host
         Write-Host "-----"
@@ -159,5 +188,5 @@ Try
 Finally
 {
     Write-Host "Closing connection to the server..."
-    bash -c "ssh -o 'ControlPath=$SocketFile' -O stop $Server"
+    & $Global:Bash -c "ssh -o 'ControlPath=$SocketFile' -O stop $Server"
 }
