31388fa8d0aa7f21c4507114c566cf630ca5e5da
Shayan Toqraee <Shayan.To@Hotmail.com>
2021-10-10 01:32:00 +0330

    Commit.

--- Commit.sh
+++ Commit.sh
@@ -1,4 +1,5 @@
-git add --all
-git commit --message "Commit." --quiet && git show HEAD --oneline --stat
+git add --all && \
+git commit --message "Commit." --quiet && \
+git show HEAD --oneline --stat && \
 git push
 read -rsn 1 -p "Press any key to continue..."
