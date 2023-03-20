404708e83a20f25bbaef282dae75f69f0ad72aca
Shayan Toqraee <Shayan.To@Hotmail.com>
2023-03-20 14:06:45 +0330

    Commit.

--- Commit.sh
+++ Commit.sh
@@ -2,5 +2,7 @@
 git add --all && \
 git commit --message "Commit." --quiet && \
 git show HEAD --oneline --stat && \
+git fetch
+git rebase origin/master
 git push
 read -rsn 1 -p "Press any key to continue..."
